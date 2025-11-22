39247d598d880b955fe945ec45d1ae76e8aa0836
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2025-11-22 21:54:33 +0100

    chore: drop previous mapper

--- src/cmap.rs
+++
@@ -1,7 +0,0 @@
-/*!
-Contains utilities for efficiently parsing and manipulating a chain file.
-*/
-
-pub mod align;
-pub mod chain;
-pub mod map;
--- src/io.rs
+++
@@ -1,7 +0,0 @@
-/*!
-Contains the IO module for the library.
-*/
-
-#[cfg(feature = "read")]
-pub mod reader;
-pub mod writer;
